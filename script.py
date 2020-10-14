ad6f59e6ed765826c9d00f35992f8044d5c9afcd
zamiraAR <72843087+zamiraAR@users.noreply.github.com>
2020-10-14 14:01:44 -0700

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,12 @@
-from bio.seq import seq
-from bio.seqFeature import seqFeature, FeatureLocation
-from Bio import seqRecord
-def summarize_contens(filename):
-    record = seqIO.read(filename, "getbank")
-    print(record)
- summarize_contents(filename)
+def summarize_contents(filename):
+       avance = SeqIO.read(filename, "genbank")
+        print("Name: ", avance.name)
+        import os
+        print ("Path: ", os.path.dirname(filename))
+       avances = list(SeqIO.parse(filename, "genbank"))
+        print("num_avances = %i avances" % len(avance))
+
+        for seq_record in SeqIO.parse(filename,"genbank"):
+                print("ID:",avance.id)
+                
+summarize_contents(filename)
