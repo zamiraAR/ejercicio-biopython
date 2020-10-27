89651f013911958c1a3bd5bb7365055600b3534f
zamiraAR <72843087+zamiraAR@users.noreply.github.com>
2020-10-27 15:18:42 -0700

    Update script.py

--- script.py
+++ script.py
@@ -6,22 +6,19 @@ def summarize_contents(filename):
 	else: 
 		type_file= "fasta"
 	record = list(SeqIO.parse(filename, type_file))
-	#Creacion de diccionario
 	d = {}
 	d['File:'] = listaOs[1]
 	d['Path:'] = listaOs[0]
 	d['Num_records:'] = len(record)
-	#Diccionario con listas
 	d['Names:'] = []
 	d['IDs:'] = []
 	d['Descriptions'] = []
-	#Registro de records
+	
 	for seq_rcd in SeqIO.parse(filename,type_file):
 		d['Names:'].append(seq_rcd.name)
 		d['IDs:'].append(seq_rcd.id)
 		d['Descriptions'].append(seq_rcd.description)
 	return d
-#Imprimir la funcion
 if _name_ == "_main_":
 	resultados = summarize_contents(filename)
 	print(resultados)
